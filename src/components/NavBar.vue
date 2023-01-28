3c1c9508a4e9ccf8cee1356df2fa3ec896ec2cb3
Gabriel Andrade <gabriel.santana@gmail.com>
2023-01-28 04:48:40 -0300

    First commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav id="nav">
+    <router-link id="logo_url" to="/">
+        <img id="logo" :src="logo" :alt="alt">
+    </router-link>
+    <router-link to="/">Home</router-link>
+    <router-link to="/pedidos">Pedidos</router-link>
+  </nav>
+</template>
+<script>
+export default {
+    name:'NavBar',
+    props:["logo","alt"]
+}
+</script>
+<style>
+    #nav {
+        background-color: #222;
+        border-bottom: 4px solid #111 ;
+        padding: 15px 50px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+
+    #nav #logo_url {
+        margin: auto;
+        margin-left: 0;
+    }
+
+    #log {
+        width: 40px;
+        height: 40px;
+    }
+
+    #nav a {
+        color: #FCBA03;
+        text-decoration: none;
+        margin:12px;
+        transition: .5s;
+    }
+
+    #nav a:hover{
+        color: #FFF;
+    }
+</style>
--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="main-container">
-    <h1>Home</h1>
+    <h1>Pedidos</h1>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
 
+export default {
+  name:'Pedidos'
+}
 </script>
+
